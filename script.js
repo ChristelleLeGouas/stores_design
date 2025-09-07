fa9f81bcf09339d620c7a1e7b73ed814068c0c88
ChristelleLeGouas <christellelegouas@gmail.com>
2025-09-07 16:56:14 +0200

    Header Footer + CSS

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function saluer() {
-    alert("Salut ! Bienvenue sur mon site.");
-  }
-  
+const overlay = document.querySelector('.overlay');
+if (overlay) {
+  overlay.addEventListener('click', () => {
+    alert('Bienvenue sur le site de notre entreprise de luxe ! ✨');
+  });
+}
+
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+if (menuToggle && menu) {
+  menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+}
